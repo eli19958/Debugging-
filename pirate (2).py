d13ea7301bfcc71c66331a641c5f66886201a860
eli19958 <98420192+eli19958@users.noreply.github.com>
2022-02-20 14:33:52 -0600

    Add files via upload

---
+++ pirate (2).py
@@ -0,0 +1,8 @@
+greeting = input("Hello, possible pirate! What's the password? ")
+if greeting in ["Arrr!"]:
+
+	print("Go away, pirate.")
+else:
+        print("Greetings, hater of pirates!")
+
+#syntax error that prevented greetings
